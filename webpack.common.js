b9fcb9e0ac2ef883c8491cac365f2c72d1de073e
AlaaDesouky <alaa.desouky33@gmail.com>
2019-06-07 10:23:55 +0200

    inital boilerplate

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: "./src/index.js",
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "img"
+          }
+        }
+      }
+    ]
+  }
+};
